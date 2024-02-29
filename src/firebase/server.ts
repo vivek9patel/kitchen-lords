22f2690a18dcf1a4004c793c5491fb08807dcbc9
Vivek Patel <vivek.p9737@gmail.com>
2024-02-29 10:38:16 -0700

    no cache for fetch requests

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -11,44 +11,44 @@ export const fetchChefById = async (id: string): Promise<Chef> => {
 }
 
 export const fetchChefByEmail = async (email: string): Promise<Chef> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/chef?email=${email}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/chef?email=${email}`,{ cache: 'no-store' });
     return response.json();
 }
 
 export const fetchChefKitchens = async (email: string): Promise<Kitchens> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/chef/kitchens?email=${email}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/chef/kitchens?email=${email}`,{ cache: 'no-store' });
     console.log("response", response.status, JSON.stringify(response) );
     return response.json();
 }
 
 export const fetchIfAuthorized = async (email: string): Promise<{isAuthorized: boolean}> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/chef/authorized?email=${email}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/chef/authorized?email=${email}`,{ cache: 'no-store' });
     console.log("response", response.status, response.statusText);
     return response.json();
 }
 
 export const fetchKitchenName = async (id: string): Promise<string> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/name?id=${id}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/name?id=${id}`,{ cache: 'no-store' });
     return response.text();
 }
 
 export const fetchKitchenWeek = async (id: string): Promise<Week> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/week?id=${id}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/week?id=${id}`,{ cache: 'no-store' });
     return response.json();
 }
 
 export const fetchKitchenDay = async (id: string, day: string): Promise<Day> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/day?id=${id}&day=${day}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/day?id=${id}&day=${day}`,{ cache: 'no-store' });
     return response.json();
 }
 
 export const fetchKitchenImage = async (id: string): Promise<string> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/image?id=${id}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/image?id=${id}`,{ cache: 'no-store' });
     return response.text();
 }
 
 export const fetchKitchenChefs = async (id: string): Promise<Chef[]> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/chefs?id=${id}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/kitchen/chefs?id=${id}`,{ cache: 'no-store' });
     return response.json();
 }
 
